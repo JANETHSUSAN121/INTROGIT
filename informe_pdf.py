4a9df62648f675151e0f5a718a0e5be2f6bba469
JANETHSUSAN121 <148640300+JANETHSUSAN121@users.noreply.github.com>
2025-09-01 20:49:05 -0500

    Update informe_pdf.py

--- informe_pdf.py
+++ informe_pdf.py
@@ -1,5 +1,4 @@
-
- import pandas as pd
+import pandas as pd
 import matplotlib.pyplot as plt
 from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Image, PageBreak
 from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
